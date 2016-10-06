97041778b3c826b253b692f735624c187ad8fc51
WiNtU <wintu520@gmail.com>
2016-10-06 15:58:56 +0900

    テスト

--- index1.php
+++ index1.php
@@ -10,6 +10,12 @@
      <script type="application/javascript">
 $(document).ready(function () {
   $("#my-calendar").zabuto_calendar({
+    action: function () {
+                return myDateFunction(this.id, false);
+            },
+            action_nav: function () {
+                return myNavFunction(this.id);
+            },
     cell_border: true,
     today: true,
     weekstartson: 0,
@@ -19,6 +25,26 @@ $(document).ready(function () {
     }
   });
 });
+function myDateFunction(id, fromModal) {
+        $("#date-popover").hide();
+        if (fromModal) {
+            $("#" + id + "_modal").modal("hide");
+        }
+        var date = $("#" + id).data("date");
+        var hasEvent = $("#" + id).data("hasEvent");
+        if (hasEvent && !fromModal) {
+            return false;
+        }
+        $("#date-popover-content").html('You clicked on date ' + date);
+        $("#date-popover").show();
+        return true;
+    }
+
+    function myNavFunction(id) {
+        $("#date-popover").hide();
+        var nav = $("#" + id).data("navigation");
+        var to = $("#" + id).data("to");
+    }
 </script>
 </head>
 <body>
