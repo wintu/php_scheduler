4120d65911f62775a0a14a176b579c74f4977aaa
WiNtU <wintu520@gmail.com>
2016-10-06 23:41:41 +0900

    fooo

--- index1.php
+++ index1.php
@@ -13,49 +13,50 @@
 <div class="row">
   <div id="my-calendar"></div>
   <script type="application/javascript">
-    $(document).ready(function () {
-        $("#date-popover").popover(...);
-        ...
-
-        $("#my-calendar").zabuto_calendar({
-            action: function () {
-                return myDateFunction(this.id, false);
-            },
-            action_nav: function () {
-                return myNavFunction(this.id);
-            },
-            ajax: {
-                url: "show_data.php?action=1",
-                modal: true
-            },
-            legend: [
-                {type: "text", label: "Special event", badge: "00"},
-                {type: "block", label: "Regular event"}
-            ]
-        });
-    });
-
-    function myDateFunction(id, fromModal) {
-        $("#date-popover").hide();
-        if (fromModal) {
-            $("#" + id + "_modal").modal("hide");
-        }
-        var date = $("#" + id).data("date");
-        var hasEvent = $("#" + id).data("hasEvent");
-        if (hasEvent && !fromModal) {
-            return false;
-        }
-        $("#date-popover-content").html('You clicked on date ' + date);
-        $("#date-popover").show();
-        return true;
-    }
-
-    function myNavFunction(id) {
-        $("#date-popover").hide();
-        var nav = $("#" + id).data("navigation");
-        var to = $("#" + id).data("to");
-    }
-</script>
+                $(document).ready(function () {
+                    $("#date-popover").popover({html: true, trigger: "manual"});
+                    $("#date-popover").hide();
+                    $("#date-popover").click(function (e) {
+                        $(this).hide();
+                    });
+                    $("#my-calendar").zabuto_calendar({
+                        action: function () {
+                            return myDateFunction(this.id, false);
+                        },
+                        action_nav: function () {
+                            return myNavFunction(this.id);
+                        },
+                        ajax: {
+                            url: "show_data.php?action=1",
+                            modal: true
+                        },
+                        legend: [
+                            {type: "text", label: "Special event", badge: "00"},
+                            {type: "block", label: "Regular event"}
+                        ]
+                    });
+                });
+                function myDateFunction(id, fromModal) {
+                    $("#date-popover").hide();
+                    if (fromModal) {
+                        $("#" + id + "_modal").modal("hide");
+                    }
+                    var date = $("#" + id).data("date");
+                    var hasEvent = $("#" + id).data("hasEvent");
+                    if (hasEvent && !fromModal) {
+                        return false;
+                    }
+                    $("#date-popover-content").html('You clicked on date ' + date);
+                    $("#date-popover").show();
+                    return true;
+                }
+                function myNavFunction(id) {
+                    $("#date-popover").hide();
+                    var nav = $("#" + id).data("navigation");
+                    var to = $("#" + id).data("to");
+                    console.log('nav ' + nav + ' to: ' + to.month + '/' + to.year);
+                }
+            </script>
 </div>
 </body>
 </html>
